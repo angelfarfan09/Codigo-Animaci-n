0fdd54757417067b6479a312ac42677f54d96515
angelfarfan09 <47245768+angelfarfan09@users.noreply.github.com>
2019-05-17 07:48:02 -0500

    Add files via upload

---
+++ GIF.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using System.Linq;
+
+public class GIF : MonoBehaviour {
+
+
+   
+   
+    public GameObject destruir;
+
+
+     Texture2D[] frames;
+     int fps = 10;
+
+
+
+
+
+    void Update()
+    {
+
+
+        int index = (int)(Time.time * fps) % frames.Length;
+        GetComponent<RawImage>().texture = frames[index];
+
+    }
+
+
+
+    public void number24()
+    {
+
+        fps = 24;
+
+        
+
+}
+
+    public void number12()
+    {
+
+         fps = 12;
+
+    }
+
+    public void number8()
+    {
+
+         fps = 8;
+
+    }
+
+
+
+
+
+   public void Play()
+    {
+
+        UnityEditor.AssetDatabase.Refresh();
+        destruir.SetActive(false);
+        
+        frames = Resources.LoadAll<Texture2D>("Textures");
+
+    }
+
+
+
+   public void Borrar()
+    {
+
+        
+        if (Directory.Exists("Assets/Resources/Textures")) { Directory.Delete("Assets/Resources/Textures", true); }
+        Directory.CreateDirectory("Assets/Resources/Textures");
+        
+       
+
+
+
+    }
+
+    
+}
+                 
+
+
+
+
+
+
+
---
+++ PantallazoHandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PantallazoHandler : MonoBehaviour
+{
+    private static PantallazoHandler instance;
+
+    private Camera miCamara;
+    private bool tomarPantallazoSiguienteFrame;
+
+    private void Awake()
+    {
+        instance = this;
+        miCamara = gameObject.GetComponent<Camera>();
+    }
+
+    private void OnPostRender()
+    {
+        if (tomarPantallazoSiguienteFrame)
+        {
+            tomarPantallazoSiguienteFrame = false;
+            RenderTexture renderTexture = miCamara.targetTexture;
+
+            Texture2D renderResult = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.ARGB32, false);
+            Rect rect = new Rect(0, 0, renderTexture.width, renderTexture.height);
+            renderResult.ReadPixels(rect, 0, 0);
+
+            byte[] byteArray = renderResult.EncodeToPNG();
+            string timeStamp = System.DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss");
+            System.IO.File.WriteAllBytes(Application.dataPath + ("/Resources/Textures/PantallazoCamara" + timeStamp + ".png"), byteArray);
+            Debug.Log("Pantallazo de Camara guardado");
+
+            RenderTexture.ReleaseTemporary(renderTexture);
+            miCamara.targetTexture = null;
+        }
+    }
+
+    private void TomarPantallazo(int width, int height)
+    {
+        miCamara.targetTexture = RenderTexture.GetTemporary(width, height, 16);
+        tomarPantallazoSiguienteFrame = true;
+    }
+
+    public static void TomarPantallazo_Static(int width, int height)
+    {
+        instance.TomarPantallazo(width, height);
+    }
+}
